d5c8abab92c1dccdf434f6cdf33a0ced33387226
Jerzy Rojek <jerzyadamrojek@gmail.com>
2020-12-13 18:11:52 +0100

    initial structure

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import TriviaMain from "./components/TriviaMain";
+
+function App() {
+  return (
+    <div className="App">
+      <TriviaMain/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TriviaMain.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TriviaMain = () => {
+    return (
+        <>
+            <h1>Hello World!</h1>
+        </>
+    );
+};
+
+export default TriviaMain;

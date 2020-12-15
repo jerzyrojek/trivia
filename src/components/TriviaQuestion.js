17f501927a5db14fbda7ef20fd4f218228c6ad0d
Jerzy Rojek <jerzyadamrojek@gmail.com>
2020-12-15 21:39:41 +0100

    sorting answers, html decoding to plain text, early click function

--- src/components/TriviaQuestion.js
+++ src/components/TriviaQuestion.js
@@ -1,14 +1,36 @@
 import React from 'react';
 
-const TriviaQuestion = ({question, correct,incorrect}) => {
-    const allAnswers = [correct, ...incorrect]
-   //need to shuffle answers and decode html to string
+const TriviaQuestion = ({question, correct, incorrect}) => {
+    const Entities = require('html-entities').AllHtmlEntities;
+    const entities = new Entities();
+    let allAnswers = [correct, ...incorrect]
+        .sort()
+        .reverse();
+    //Making sure the order of answers is always the same for True/False questions
+
+    allAnswers.forEach(answer => entities.decode(answer));
+
+    if (allAnswers.length > 2) {
+        allAnswers = allAnswers.sort(() => {
+            return 0.5 - Math.random()
+        });
+    }
+    //shuffling answers if there are more than 2, so the right one isn't always in the same place
+
+    const handleOnClick = (e) => {
+        if (e.target.value === correct) {
+            console.log("Dobra odpowiedź")
+        } else {
+            console.log("Zła odpowiedź")
+        }
+    }
+
 
     return (
         <>
-            <p>{question}</p>
-            {allAnswers.map ((el, index) => {
-                return <span key={index}>{el}</span>
+            <p>{entities.decode(question)}</p>
+            {allAnswers.map((el, index) => {
+                return <button value={el} onClick={handleOnClick} key={index}>{el} </button>
             })}
 
         </>

9ea54e847be0a515ba74006ba4158f6d49fb9a6a
Jerzy Rojek <jerzyadamrojek@gmail.com>
2020-12-15 21:38:51 +0100

    minor change and comments

--- src/components/TriviaMain.js
+++ src/components/TriviaMain.js
@@ -4,7 +4,9 @@ import TriviaQuestion from "./TriviaQuestion";
 const TriviaMain = () => {
     const [questions, setQuestions] = useState(null);
     // const [sessionToken, setSessionToken] = useState(null);
+    // const [score, setScore] = useState(null);
     // need to add session token to local storage to avoid the same questions being pulled from API
+    //score should also be stored in local storage
     //https://opentdb.com/api_token.php?command=request
 
     useEffect(() => {
@@ -25,9 +27,10 @@ const TriviaMain = () => {
 
     return (
         <>
-            <h1>Hello World!</h1>
-            {questions && questions.map((el,index) => {
-                return <TriviaQuestion key={index} question={el.question} correct={el.correct_answer} incorrect={el.incorrect_answers}/>
+            <h1>Game Trivia!</h1>
+            {questions && questions.map((el, index) => {
+                return <TriviaQuestion key={index} question={el.question} correct={el.correct_answer}
+                                       incorrect={el.incorrect_answers}/>
             })}
         </>
     );

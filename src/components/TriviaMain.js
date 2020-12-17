22ac28630f7174382c0c0a6a51ee37c6c163d1a7
Jerzy Rojek <jerzyadamrojek@gmail.com>
2020-12-17 20:20:59 +0100

    passing token to local storage

--- src/components/TriviaMain.js
+++ src/components/TriviaMain.js
@@ -3,15 +3,35 @@ import TriviaQuestion from "./TriviaQuestion";
 
 const TriviaMain = () => {
     const [questions, setQuestions] = useState(null);
-    // const [sessionToken, setSessionToken] = useState(null);
-    // const [score, setScore] = useState(null);
+    const [score, setScore] = useState(null);
+    const [sessionToken, setSessionToken] = useState();
     // need to add session token to local storage to avoid the same questions being pulled from API
     //score should also be stored in local storage
     //https://opentdb.com/api_token.php?command=request
 
     useEffect(() => {
+        setSessionToken(
+            JSON.parse(localStorage.getItem("sessionInfo")).token
+        )
+        if (localStorage.getItem("sessionInfo") === null) {
+            fetch("https://opentdb.com/api_token.php?command=request").then(response =>
+                response.json().then(res => {
+                    console.log(res)
+                    localStorage.setItem(
+                        "sessionInfo",
+                        JSON.stringify({
+                            token: res.token,
+                        })
+                    );
+                })
+            ).then()
+        }
+    },[])
+
+
+    useEffect(() => {
         let mounted = true;
-        fetch("https://opentdb.com/api.php?amount=10&category=15").then(response =>
+        fetch(`https://opentdb.com/api.php?amount=10&category=15&token=${sessionToken}`).then(response =>
             response.json().then(res => {
                     if (mounted) {
                         setQuestions(res.results);
@@ -23,7 +43,7 @@ const TriviaMain = () => {
             mounted = false;
         }
 
-    }, [])
+    }, [sessionToken])
 
     return (
         <>

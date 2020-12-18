678cf68e0083ce2cc0ab30bb4cf7bb0a02f11a88
Jerzy Rojek <jerzyadamrojek@gmail.com>
2020-12-18 20:40:43 +0100

    early score logic

--- src/components/TriviaMain.js
+++ src/components/TriviaMain.js
@@ -3,17 +3,17 @@ import TriviaQuestion from "./TriviaQuestion";
 
 const TriviaMain = () => {
     const [questions, setQuestions] = useState(null);
-    const [score, setScore] = useState(null);
+    const [score, setScore] = useState(0);
     const [sessionToken, setSessionToken] = useState();
     // need to add session token to local storage to avoid the same questions being pulled from API
     //score should also be stored in local storage
     //https://opentdb.com/api_token.php?command=request
 
     useEffect(() => {
-        setSessionToken(
-            JSON.parse(localStorage.getItem("sessionInfo")).token
-        )
-        if (localStorage.getItem("sessionInfo") === null) {
+        if (localStorage.getItem("sessionInfo") !== null) {
+            setSessionToken(
+                JSON.parse(localStorage.getItem("sessionInfo")).token)
+        } else {
             fetch("https://opentdb.com/api_token.php?command=request").then(response =>
                 response.json().then(res => {
                     console.log(res)
@@ -24,9 +24,9 @@ const TriviaMain = () => {
                         })
                     );
                 })
-            ).then()
+            )
         }
-    },[])
+    }, [])
 
 
     useEffect(() => {
@@ -45,12 +45,21 @@ const TriviaMain = () => {
 
     }, [sessionToken])
 
+    const handleChangeScore = () => {
+        setScore(prev => prev + 1);
+    }
+
+    // const resetScore = () => {
+    //
+    // }
+
     return (
         <>
             <h1>Game Trivia!</h1>
+            <h2>Current score: {score}</h2>
             {questions && questions.map((el, index) => {
                 return <TriviaQuestion key={index} question={el.question} correct={el.correct_answer}
-                                       incorrect={el.incorrect_answers}/>
+                                       incorrect={el.incorrect_answers} updateScore={handleChangeScore}/>
             })}
         </>
     );
--- src/components/TriviaQuestion.js
+++ src/components/TriviaQuestion.js
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-const TriviaQuestion = ({question, correct, incorrect}) => {
+const TriviaQuestion = ({question, correct, incorrect, updateScore}) => {
     const Entities = require('html-entities').AllHtmlEntities;
     const entities = new Entities();
+    const [chosen, setChosen] = useState(false);
+
     let allAnswers = [correct, ...incorrect]
         .sort()
         .reverse();
     //Making sure the order of answers is always the same for True/False questions
 
-    allAnswers.forEach(answer => entities.decode(answer));
+    useEffect(() => {
+        allAnswers.forEach(answer => entities.decode(answer));
+        if (allAnswers.length > 2) {
+            allAnswers = allAnswers.sort(() => {
+                return 0.5 - Math.random()
+            });
+        }
+        //shuffling answers if there are more than 2, so the right one isn't always in the same place
+    }, [question])
 
-    if (allAnswers.length > 2) {
-        allAnswers = allAnswers.sort(() => {
-            return 0.5 - Math.random()
-        });
-    }
-    //shuffling answers if there are more than 2, so the right one isn't always in the same place
 
     const handleOnClick = (e) => {
+        setChosen(true);
         if (e.target.value === correct) {
             console.log("Dobra odpowiedź")
+            updateScore();
         } else {
             console.log("Zła odpowiedź")
         }
@@ -30,7 +36,7 @@ const TriviaQuestion = ({question, correct, incorrect}) => {
         <>
             <p>{entities.decode(question)}</p>
             {allAnswers.map((el, index) => {
-                return <button value={el} onClick={handleOnClick} key={index}>{el} </button>
+                return <button disabled={chosen} value={el} onClick={handleOnClick} key={index}>{el} </button>
             })}
 
         </>

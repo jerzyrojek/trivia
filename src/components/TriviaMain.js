3a988cbcc40f223f152ec243eba93b5d15cc6025
Jerzy Rojek <jerzyadamrojek@gmail.com>
2020-12-21 17:57:07 +0100

    getting new token if old one expires

--- src/components/TriviaMain.js
+++ src/components/TriviaMain.js
@@ -4,12 +4,16 @@ import TriviaQuestion from "./TriviaQuestion";
 const TriviaMain = () => {
     const [questions, setQuestions] = useState(null);
     const [score, setScore] = useState(0);
-    const [sessionToken, setSessionToken] = useState();
-    // need to add session token to local storage to avoid the same questions being pulled from API
+    const [sessionToken, setSessionToken] = useState(null);
     //score should also be stored in local storage
-    //https://opentdb.com/api_token.php?command=request
 
     useEffect(() => {
+        if(questions && questions.response_code === 3){
+            localStorage.removeItem(
+                "sessionInfo",
+            )
+        }
+
         if (localStorage.getItem("sessionInfo") !== null) {
             setSessionToken(
                 JSON.parse(localStorage.getItem("sessionInfo")).token)
@@ -17,6 +21,7 @@ const TriviaMain = () => {
             fetch("https://opentdb.com/api_token.php?command=request").then(response =>
                 response.json().then(res => {
                     console.log(res)
+                    setSessionToken(res.token);
                     localStorage.setItem(
                         "sessionInfo",
                         JSON.stringify({
@@ -26,15 +31,16 @@ const TriviaMain = () => {
                 })
             )
         }
-    }, [])
+    }, [questions])
 
 
     useEffect(() => {
         let mounted = true;
         fetch(`https://opentdb.com/api.php?amount=10&category=15&token=${sessionToken}`).then(response =>
             response.json().then(res => {
+                console.log(res)
                     if (mounted) {
-                        setQuestions(res.results);
+                        setQuestions(res);
                     }
                 }
             ))
@@ -57,7 +63,7 @@ const TriviaMain = () => {
         <>
             <h1>Game Trivia!</h1>
             <h2>Current score: {score}</h2>
-            {questions && questions.map((el, index) => {
+            {questions && questions.results.map((el, index) => {
                 return <TriviaQuestion key={index} question={el.question} correct={el.correct_answer}
                                        incorrect={el.incorrect_answers} updateScore={handleChangeScore}/>
             })}

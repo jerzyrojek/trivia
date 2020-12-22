9fdf9f2514676bee76c3dba0e22f3699b1d34cf0
Jerzy Rojek <jerzyadamrojek@gmail.com>
2020-12-22 19:31:30 +0100

    loading more questions, plus overall number of points

--- src/components/TriviaMain.js
+++ src/components/TriviaMain.js
@@ -3,14 +3,23 @@ import TriviaQuestion from "./TriviaQuestion";
 
 const TriviaMain = () => {
     const [questions, setQuestions] = useState(null);
-    const [score, setScore] = useState(JSON.parse(localStorage.getItem("currentScore")).points);
+    const [score, setScore] = useState(0);
+    const [totalPointsPool, setTotalPointsPool] = useState(10);
     const [sessionToken, setSessionToken] = useState(null);
+
 
     useEffect(() => {
         if (questions && questions.response_code === 3) {
             localStorage.removeItem(
                 "sessionInfo",
             )
+        }
+        if(localStorage.getItem("currentScore") !== null) {
+            setScore(JSON.parse(localStorage.getItem("currentScore")).points)
+        }
+
+        if(localStorage.getItem("total") !== null) {
+            setTotalPointsPool(JSON.parse(localStorage.getItem("total")).totalPointsAmount)
         }
 
         if (localStorage.getItem("sessionInfo") !== null) {
@@ -19,7 +28,6 @@ const TriviaMain = () => {
         } else {
             fetch("https://opentdb.com/api_token.php?command=request").then(response =>
                 response.json().then(res => {
-                    console.log(res)
                     setSessionToken(res.token);
                     localStorage.setItem(
                         "sessionInfo",
@@ -37,7 +45,6 @@ const TriviaMain = () => {
         let mounted = true;
         fetch(`https://opentdb.com/api.php?amount=10&category=15&token=${sessionToken}`).then(response =>
             response.json().then(res => {
-                    console.log(res)
                     if (mounted) {
                         setQuestions(res);
                     }
@@ -48,7 +55,7 @@ const TriviaMain = () => {
             mounted = false;
         }
 
-    }, [sessionToken])
+    }, [sessionToken, totalPointsPool])
 
     useEffect(() => {
         localStorage.setItem(
@@ -57,10 +64,22 @@ const TriviaMain = () => {
                 points: score,
             })
         );
-    },[score])
+
+        localStorage.setItem(
+            "total",
+            JSON.stringify({
+                totalPointsAmount: totalPointsPool,
+            })
+        );
+
+    }, [score, totalPointsPool])
 
     const handleChangeScore = () => {
         setScore(prev => prev + 1);
+    }
+
+    const nextTenQuestions = () => {
+        setTotalPointsPool(prev => prev + 10)
     }
 
     const resetSession = () => {
@@ -70,23 +89,33 @@ const TriviaMain = () => {
         localStorage.setItem(
             "currentScore",
             JSON.stringify({
-                points:0
+                points: 0
+            })
+        )
+        localStorage.setItem(
+            "total",
+            JSON.stringify({
+                totalPointsAmount: 10
             })
         )
         setScore(0);
+        setTotalPointsPool(10);
         setSessionToken(null);
     }
 
     return (
         <>
             <h1>Game Trivia!</h1>
-            <h2>Current score: {score}</h2>
+            <h2>Current score: {score}/{totalPointsPool}</h2>
             {questions && questions.results.map((el, index) => {
                 return <TriviaQuestion key={index} question={el.question} correct={el.correct_answer}
                                        incorrect={el.incorrect_answers} updateScore={handleChangeScore}/>
             })}
             <div>
                 <button onClick={resetSession}>Reset</button>
+            </div>
+            <div>
+                <button onClick={nextTenQuestions}>Next 10</button>
             </div>
         </>
     );

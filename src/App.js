94a8a11ac8fb2bd69b474a785a79b9e062a28358
Jerzy Rojek <jerzyadamrojek@gmail.com>
2021-01-11 18:19:27 +0100

    routing setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React from 'react';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import * as ROUTES from "./constants/routes";
 import TriviaMain from "./components/TriviaMain";
+import ResultPage from "./components/ResultPage";
+
 
 function App() {
   return (
     <div className="App">
-      <TriviaMain/>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path={ROUTES.HOME} component={TriviaMain}/>
+                <Route path={ROUTES.RESULTS} component={ResultPage}/>
+            </Switch>
+        </BrowserRouter>
     </div>
   );
 }
